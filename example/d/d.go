71dc12d409f1c8ad5288c5ede9804d2214114bb8
Christopher Dunn <cdunn2001@gmail.com>
2011-07-23 14:45:22 -0700

    Added example for target in a non-namespaced sub-dir.

--- example/d/d.go
+++ example/d/d.go
@@ -4,11 +4,13 @@ import (
        "different/a"
        "different/a/b"
 	   "e"
+       "g"
 )
 
 func main() {
      a.AFoo()
 	 b.BFoo()
+     g.GFoo()
 	 println(e.CSin(2.4))
 	 println(e.Atoi("5"))
 }
