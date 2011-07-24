b4a7d7f5b5eea93ea7a3c83f710e40a2f8ef3818
Christopher Dunn <cdunn2001@gmail.com>
2011-07-23 22:52:15 -0700

    Fixed typos and indentation.

--- example/d/d.go
+++ example/d/d.go
@@ -3,13 +3,13 @@ package main
 import (
        "different/a"
        "different/a/b"
-	   "e"
+       "e"
        "g"
 )
 
 func main() {
      a.AFoo()
-	 b.BFoo()
+     b.BFoo()
      g.GFoo()
 	 println(e.CSin(2.4))
 	 println(e.Atoi("5"))
