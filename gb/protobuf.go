f8b26d4d2326dbcb6f242576bf17af2bd0eb5a3b
John Asmuth <jasmuth@gmail.com>
2011-10-20 15:54:33 -0400

    -m

---
+++ gb/protobuf.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"path/filepath"
+	"os"
+	"fmt"
+)
+
+func GoForProto(protosrc string) (gosrc string) {
+	base := protosrc[:len(protosrc)-len(".proto")]
+	gosrc = base+".pb.go"
+	return
+}
+
+func GenerateProtobufSource(this *Package) (err os.Error) {
+	for _, pbs := range this.ProtoSrcs {
+		args := []string{"protoc", "--go_out=.", pbs}
+		if Verbose {
+			fmt.Println(args)
+		}
+		err = RunExternal(ProtocCMD, this.Dir, args)
+		if err != nil {
+			return
+		}
+
+		gosrc := GoForProto(pbs)
+
+		var protopkg string
+		protopkg, _, _, _, _, _, err = GetDeps(filepath.Join(this.Dir, gosrc))
+		if err != nil {
+			return
+		}
+
+		this.PkgSrc[protopkg] = append(this.PkgSrc[protopkg], gosrc)
+	}
+	return
+}
