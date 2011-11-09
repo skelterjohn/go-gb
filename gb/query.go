c93e2467abc4098a05f014c5735778e29558b813
John Asmuth <jasmuth@gmail.com>
2011-11-09 14:00:09 -0500

    fixed bug where workspace.gb messed things up if it was in a gopath target

--- gb/query.go
+++ gb/query.go
@@ -35,14 +35,12 @@ var GOPATHS, GOPATH_SRCROOTS, GOPATH_OBJDSTS, GOPATH_CFLAGS, GOPATH_LDFLAGS []st
 func LoadCWD() (err error) {
 	var oserr error
 	OSWD, oserr = os.Getwd()
-	rel, relerr := ReadOneLine("workspace.gb")
 
 	CWD, err = OSWD, oserr
 
-	if relerr == nil {
-		CWD = GetAbs(filepath.Join(OSWD, rel), OSWD)
-		fmt.Printf("Running gb in workspace %s\n", CWD)
-	} else if GOPATH = os.Getenv("GOPATH"); GOPATH != "" {
+	runningInGOPATH := false
+
+	if GOPATH = os.Getenv("GOPATH"); GOPATH != "" {
 		gopaths := filepath.SplitList(GOPATH)
 		for _, gp := range gopaths {
 			gp = strings.TrimSpace(gp)
@@ -56,13 +54,21 @@ func LoadCWD() (err error) {
 				if CWD != gpsrc {
 					CWD = gpsrc
 					fmt.Printf("Running gb in GOPATH workspace %s\n", CWD)
-					os.Chdir(CWD)
+					runningInGOPATH = true
 				}
 			}
 		}
 	}
 
+	if !runningInGOPATH {
+		rel, relerr := ReadOneLine("workspace.gb")
+		if relerr == nil {
+			CWD = GetAbs(filepath.Join(OSWD, rel), OSWD)
+			fmt.Printf("Running gb in workspace %s\n", CWD)
+		}
+	}
 	os.Chdir(CWD)
+
 	return
 }
 
