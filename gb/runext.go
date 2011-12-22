05f04dfc0b1b4d0b6c2f1cf461aaeaea69fb15a1
John Asmuth <jasmuth@gmail.com>
2011-12-22 11:09:41 -0500

    split arg fields with strings.Fields()

--- gb/runext.go
+++ gb/runext.go
@@ -111,7 +111,7 @@ func FindExternals() (err error) {
 
 func SplitArgs(args []string) (sargs []string) {
 	for _, arg := range args {
-		sarg := strings.Split(arg, " ")
+		sarg := strings.Fields(arg)
 		sargs = append(sargs, sarg...)
 	}
 	return
